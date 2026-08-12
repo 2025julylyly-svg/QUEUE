03c3dff20463526306b5007805be71260fe29544
Develepment <2025julylyly@gmail.com>
2026-08-12 15:17:15 +0330

    add constructors

--- Library/QueueFiles/Queue.h
+++ Library/QueueFiles/Queue.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <initializer_list>
 
 template <typename value>
 class Queue
@@ -10,4 +11,13 @@ private:
     using const_reference = const value&;
 private:
     pointer queue;
+public:
+    // default constructor
+    explicit Queue();
+    // init constructor
+    explicit Queue(std::initializer_list<value>);
+    // copy constructor
+    explicit Queue(const Queue&);
+    // move constructor
+    explicit Queue(Queue&&);
 };
