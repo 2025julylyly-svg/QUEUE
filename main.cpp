2e2509e3838772e6af427b83c0271a704890a1d3
Develepment <2025julylyly@gmail.com>
2026-08-12 18:06:23 +0330

    final project

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 #include "Library/QueueFiles/MAIN/Queue.h"
 
 int main() {
-    Queue<int> q;
-    for (int i = 0; i <= 10; i += 2) {
-        q.Add( i );
-    }
-    q.Remove();
-    for (const int* it = &q.Front(); it != &q.Back() + 1; ++it) {
+    Queue<double> q;
+    q.Add( 1.1 );
+    q.Add( 2.2 );
+    q.Add( 3.3 );
+    q.Add( 4.4 );
+    q.Add( 5.5 );
+    q.Add( 6.6 );
+    q.Add( 7.7 );
+    q.Add( 8.8 );
+    q.Add( 9.9 );
+    for (const double* it = &q.Front(); it != &q.Back() + 1; ++it) {
         std::cout << *it << std::endl;
     }
     return 0;
